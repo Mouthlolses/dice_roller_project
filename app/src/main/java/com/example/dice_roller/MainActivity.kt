9ca544f457fbd983fa0378adc923e94e9e701905
Matheus <140640590+Mouthlolses@users.noreply.github.com>
2024-09-30 04:41:01 -0300

    round1

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -38,37 +38,37 @@ class MainActivity : ComponentActivity() {
     }
 
 
+
+    @Composable
+    fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+        var result by remember { mutableStateOf(1) }
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = result.toString()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { result = (1..6).random() }) {
+                Text(text = stringResource(id = R.string.roll))
+            }
+        }
+    }
+
 @Preview(showBackground = true)
 @Composable
 fun DiceRollerApp() {
-        DiceWithButtonAndImage()
-}
-
-
-@Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-    Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result =(1..6).random() }) {
-            Text(text = stringResource(id = R.string.roll))
-        }
-    }
+    DiceWithButtonAndImage()
 }
